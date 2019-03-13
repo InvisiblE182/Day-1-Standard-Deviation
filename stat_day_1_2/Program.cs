8026ead6f3be85f3e3ad08dd6a5e389c4d620a89
InvisiblE182 <osobnimail@protonmail.com>
2019-03-13 20:25:33 +0100

    Add project files.

---
+++ stat_day_1_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stat_day_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] intInput = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+            double mean = intInput.Average();
+
+            List<double> sqDist = new List<double>();
+
+            for (int i = 0; i < intInput.Length; i++)
+            {
+                sqDist.Add(Math.Pow(Convert.ToDouble((intInput[i] - mean)), 2.0));
+            }
+            foreach (double d in sqDist)
+            {
+                Console.WriteLine(d);
+            }
+            Console.WriteLine(string.Format("{0:F1}", Math.Sqrt(sqDist.Sum() / n)));
+            Console.ReadLine();
+        }
+    }
+}
